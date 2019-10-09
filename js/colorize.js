a8bb0a3243ecc993fbc65fb877833863ea71552b
kuzmich84 <kudim@inbox.ru>
2019-10-09 11:19:07 +0300

    correct module colorize.js

--- js/colorize.js
+++ js/colorize.js
@@ -1,12 +1,15 @@
 'use strict';
 
 (function () {
-  window.colorize = function (element, color) {
+  window.colorize = function (element, color, inputName) {
     element.addEventListener('click', function () {
       if (element.tagName.toLowerCase() === 'div') {
-        element.style.background = window.util.getRandomElement(color);
+        var currentColor = window.util.getRandomElement(color);
+        element.style.background = currentColor;
+        inputName.value = currentColor;
       } else {
         element.style.fill = window.util.getRandomElement(color);
+        inputName.value = element.style.fill;
       }
     });
   };
